f18e0312c8f670ae8a315437d6a9b4ddedf28763
bhargavthegr8 <73594912+bhargavthegr8@users.noreply.github.com>
2021-05-02 13:00:13 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,17 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.trajectory = [];
+    this.smokeImage= loadImage("sprites/smoke.png")
+  }
+
+  display() {
+    super.display();
+    var pos = [this.body.position.x,this.body.position.y];
+    this.trajectory.push(pos);
+    for(var i=0; i<this.trajectory.length; i++){
+    image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1])
+    }
+  }
+}
